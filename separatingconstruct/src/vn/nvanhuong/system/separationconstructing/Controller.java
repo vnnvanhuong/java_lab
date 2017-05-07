44d8c88d661ba364dc7f02b964628828852c179c
Van Huong <vnnvanhuong@gmail.com>
2017-05-07 20:49:49 +0700

    separatingconstruct: init service in getService method

--- separatingconstruct/src/vn/nvanhuong/system/separationconstructing/Controller.java
+++ separatingconstruct/src/vn/nvanhuong/system/separationconstructing/Controller.java
@@ -3,14 +3,15 @@ package vn.nvanhuong.system.separationconstructing;
 public class Controller {
 	private Service service;
 	
-	public Controller(){
+	public void doAction(){
+		System.out.println("doAction in Controller");
+		getService().execute();
+	}
+	
+	public Service getService(){
 		if(service == null){
 			service = new DefaultService();
 		}
-	}
-	
-	public void doAction(){
-		System.out.println("doAction in Controller");
-		service.execute();
+		return service;
 	}
 }
