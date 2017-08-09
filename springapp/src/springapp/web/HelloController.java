b23b8198b9e42421fc957af2631817cb200c503c
vnnvanhuong <vnnvanhuong@gmail.com>
2017-08-09 21:40:36 +0700

    init project for learning spring framework

---
+++ springapp/src/springapp/web/HelloController.java
@@ -0,0 +1,21 @@
+package springapp.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller{
+	protected final Log logger = LogFactory.getLog(getClass());
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		logger.info("Returning hello view");
+	
+		return new ModelAndView("hello.jsp");
+	}
+
+}
