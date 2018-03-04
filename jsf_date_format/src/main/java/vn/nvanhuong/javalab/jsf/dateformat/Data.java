7bea0c9f557992ebc760894b931841dc29574888
Huong Nguyen <huong.nguyen@ubitec.ch>
2018-03-04 20:50:27 +0700

    jsf_date_format: initial project

---
+++ jsf_date_format/src/main/java/vn/nvanhuong/javalab/jsf/dateformat/Data.java
@@ -0,0 +1,18 @@
+package vn.nvanhuong.javalab.jsf.dateformat;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean(name = "data")
+@SessionScoped
+public class Data {
+	private String greeting = "Hello JSF";
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+}
